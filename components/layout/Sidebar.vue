2a1c804cae6d8cfda1a1f4119892faea92360907
Viktor Filippov <jackal3166@gmail.com>
2024-03-18 00:42:10 +0200

    Add favicon

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-	<aside class="relative w-full h-full px-5 py-8">
+	<aside class="relative w-full h-full px-5 py-8 bg-slate-900">
 		<NuxtLink to="/" class="block mb-10"
-			><NuxtImg src="/hand.svg" alt="Logo" width="100px" class="mx-auto"
+			><NuxtImg src="/vuejs.svg" alt="Logo" width="100px" class="mx-auto"
 		/></NuxtLink>
-
-		<UiButton>Send</UiButton>
+		<button class="absolute duration-300 top-3 right-3 text-slate-400 hover:text-amber-700">
+			<Icon name="line-md:logout" size="24" />
+		</button>
 	</aside>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			title: 'CRM ViKTor',
+			meta: [
+				{ name: 'description', content: 'The CRM for the my Company' },
+			],
+			link: [{ rel: 'icon', type: 'image/svg+xml', href: '/pinia.svg' }],
+		},
+	},
 	devtools: { enabled: false },
 	modules: [
 		'@nuxtjs/tailwindcss',
 		'shadcn-nuxt',
 		'@nuxt/image',
 		'@pinia/nuxt',
+		'@nuxtjs/google-fonts',
+		'nuxt-icon',
 	],
 	shadcn: {
 		prefix: 'Ui',
 		componentDir: './components/ui',
+	},
+	googleFonts: {
+		families: {
+			'Montserrat+Alternates': {
+				wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+			},
+		},
 	},
 });
