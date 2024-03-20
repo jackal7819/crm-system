04c872d9c28130bd76103228fc1bc1da9731fc7c
Viktor Filippov <jackal3166@gmail.com>
2024-03-20 16:03:40 +0200

    Add pages

---
+++ pages/customers.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="min-h-screen p-10 bg-slate-800">
+		<h1 class="mb-10 text-3xl font-bold text-center text-slate-400">
+			Customers
+		</h1>
+	</div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+	useSeoMeta({
+		title: 'Login | CRM System',
+	});
+</script>
+
+<template>
+	<div class="flex items-center justify-center min-h-screen bg-slate-800">
+		<div class="p-10 rounded-lg bg-slate-900 text-slate-400">
+			<h1 class="mb-10 text-3xl font-bold text-center">Login</h1>
+			<form>
+				<UiInput
+					placeholder="Email"
+					label="Email"
+					type="email"
+					class="mb-3"
+				/>
+			</form>
+		</div>
+	</div>
+</template>
