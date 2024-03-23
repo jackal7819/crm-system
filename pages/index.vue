9f05c4bb4e8627f6441b5206e7f9373b4bb4e4b6
Viktor Filippov <jackal3166@gmail.com>
2024-03-24 00:27:19 +0200

    Add SeoMeta

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+	useSeoMeta({
+		title: 'Home | CRM System',
+	})
+</script>
+
 <template>
 	<div class="h-screen p-10 bg-slate-800 text-slate-400">
 		<h1 class="mb-10 text-2xl font-bold">CRM System by ViKTor</h1>
